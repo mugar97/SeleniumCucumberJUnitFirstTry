0340d583bb830818f58f69990a7cecc4b7859070
Cristhian Murcia <mugar97@gmail.com>
2021-05-01 22:23:04 -0500

    cleaning initial structure

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", glue = {"stepDefinitions","hooks"})
+@CucumberOptions(features = "src/test/resources", glue = "stepDefinitions")
 public class TestRunner {
 
 }
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,4 +1,4 @@
-package hooks;
+package stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
